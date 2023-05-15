becb2fdb6c133fdd2d451b402742dcf190181b9b
olili27 <timmolili27@gmail.com>
2023-05-15 11:22:05 +0300

    provide constructor

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("orders")
 public class OrderController {
 
+    public OrderController() {}
+
 //    @Autowired
     OrderService orderService = new OrderService();
 
